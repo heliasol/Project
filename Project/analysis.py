6dde33fd7a763656ba0a2f7035d68e021bd37b90
heliasol <heliasoltanijoo@gmail.com>
2026-02-18 22:17:06 +0100

    stat added 02.18 22:17

--- Project/analysis.py
+++ Project/analysis.py
@@ -116,3 +116,104 @@ class GeneAnalyser:
                     return msca   # return the first common ancestor found
 
         return None
+
+    class NumericalAnalysis(ABC):
+    def __init__(self, ontology_df : OBOParser, annotation_df : GAFParser):
+        self._ontology = ontology_df
+        self._annotations = annotation_df
+
+    @property
+    @abstractmethod
+    def compute(self):
+        raise NotImplementedError("You can't instanciate this object -> ABSTRACT!")
+
+
+class SummaryStatistics(NumericalAnalysis):
+    def __init__(self, ontology_df, annotation_df, term: TermCollection):
+        super().__init__(ontology_df, annotation_df)
+        self.__term = term
+        
+    @property
+    def compute(self):
+        onto_df = self._ontology.copy()
+        onto_df["n_parents"] = onto_df["parents"].apply(len)
+       
+        def get_children_info(go_id):
+            children = self.__term.get_children(go_id)
+            return pd.Series({
+                "n_children": len(children),
+                "is_leaf": len(children) == 0
+            })
+        
+        # APPLICA UNA SOLA VOLTA
+        children_info = onto_df["go_id"].apply(get_children_info)
+        onto_df["n_children"] = children_info["n_children"]
+        onto_df["is_leaf"] = children_info["is_leaf"]
+        leaf_count = onto_df["is_leaf"].sum()
+        ann_df = self._annotations.copy()
+
+        experimental_codes = {"EXP","IDA","IPI","IMP","IGI","IEP"}
+        ann_df["is_experimental"] = ann_df["evidence"].isin(experimental_codes)
+        
+        return {
+            "namespace counts": onto_df["namespace"].value_counts(),
+            "avg parents": onto_df["n_parents"].mean(),
+            "avg children": onto_df["n_children"].mean(),
+            "leaf_percentage": str(leaf_count / len(onto_df) * 100) + "%",
+            "evidence counts": ann_df["evidence"].value_counts(),
+            "experimental vs computational": ann_df["is_experimental"].value_counts()
+        }
+    
+    def plots(self):
+        summary = self.compute
+    
+        plt.figure() # Crea una pagina bianca nuova
+        summary["namespace counts"].plot(kind="bar", title="Namespace") #ma qua me lo divide per nomi o non me li scrive?
+        
+        plt.figure() # Crea un'altra pagina bianca nuova
+        summary["evidence counts"].plot(kind="bar", title="Evidence")
+        
+        plt.figure() # Crea la terza pagina bianca
+        summary["experimental vs computational"].plot(kind="bar", title="Exp vs Comp")
+        
+        plt.show() # Mostra tutto
+
+'''principle: the more go_term 2 gene share the more they are related'''
+class GeneSimilarityAnalysis(NumericalAnalysis):
+    def __init__(self, ontology_df, annotation_df):
+        super().__init__(ontology_df, annotation_df)
+        self.__sim = None  #so it doesn't get built from scratch every time
+
+    @property
+    def compute(self):
+        """
+        Returns gene-gene Jaccard similarity matrix
+        """
+        if self.__sim is not None:
+            return self.__sim     #if it is already built it just returns it
+
+        # gene × term table (binary)   1 se gena ha quel GO c'è - 0 se non lo ha  #cross tab do so
+        table = pd.crosstab(
+            self._annotations["gene_id"],
+            self._annotations["go_id"]
+        )
+
+        # convert to numpy, bacause it's easier to work with matricial and logical operations
+        M = table.values
+
+        n = M.shape[0]
+        sim = np.zeros((n, n))
+
+        for i in range(n):
+            for j in range(n):
+                intersection = np.logical_and(M[i], M[j]).sum()
+                union = np.logical_or(M[i], M[j]).sum()
+
+                sim[i, j] = intersection / union if union else 0  #jaccard similarity -> a stats method to compare two sets
+
+        self.__sim = pd.DataFrame(sim, index=table.index, columns=table.index)  #it returns the matrix with labled col and rows(all genes)
+        return self.__sim
+
+    def compare2genes(self, gene1, gene2):
+        return self.compute.at[gene1,gene2]
+
